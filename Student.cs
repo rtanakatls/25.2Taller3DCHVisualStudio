c890a8d3ea75a26d04c11230797438e2cba408c8
Ricardo Tanaka Terukina <rtanaka92@gmail.com>
2025-08-28 18:05:26 -0500

    Created project

---
+++ Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller3D252Virtual
+{
+    internal class Student
+    {
+        private string name;
+        private int age;
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+        }
+
+        public Student(string name)
+        {
+            this.name = name;
+        }
+
+        public void SetAge(int age)
+        {
+            this.age = age;
+        }
+
+    }
+}
